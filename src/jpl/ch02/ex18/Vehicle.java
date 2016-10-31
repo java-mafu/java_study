a8e8f67f1b9674076569a2f2441f080971a0e3bd
fumihiro teshima <teshima.java.program@gmail.com>
2016-11-01 00:30:08 +0900

    ex17,18 update

--- src/jpl/ch02/ex18/Vehicle.java
+++ src/jpl/ch02/ex18/Vehicle.java
@@ -82,7 +82,7 @@ class Vehicle {
 
 	public static void main(String args[]){
 		if(args.length ==0){
-			System.out.println("There are no cars");
+			System.out.println("There are no cars.");
 		}
 			for (String string : args) {
 				Vehicle car = new Vehicle();
