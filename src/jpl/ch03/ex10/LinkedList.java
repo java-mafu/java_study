7039364695d075c73c91574b8bca61e51ee07f19
fumihiro teshima <teshima.java.program@gmail.com>
2016-11-27 11:24:42 +0900

    ch03.ex10作成．しかし，自信がないので，editに追加

---
+++ src/jpl/ch03/ex10/LinkedList.java
@@ -0,0 +1,69 @@
+package jpl.ch03.ex10;
+import jpl.ch02.ex10.Vehicle;
+
+class LinkedList {
+	private Object list;
+	private LinkedList nextlink;
+	static private int num = 0;
+
+	LinkedList(){
+		this.list = null;
+		this.nextlink = null;
+	}
+
+	LinkedList(Object list){
+		this();
+		this.list = list;
+	}
+
+	LinkedList(Object list, LinkedList nextlink){
+		this.list = list;
+		this.nextlink = nextlink;
+	}
+
+	public String toString() {
+		String desc = list.toString();
+		if (nextlink != null){
+			desc += " nextlink "+ nextlink.toString();
+		}
+		return desc;
+	}
+
+	public Object getList() {
+		return list;
+	}
+	public void setList(Object list) {
+		this.list = list;
+	}
+	public LinkedList getNextLink() {
+		return nextlink;
+	}
+
+	public int totalLinkNum(){
+		num++;
+		int totalnum = num;
+
+		if (nextlink != null)
+			 totalnum = nextlink.totalLinkNum();
+		else {
+			totalnum = num;
+			num = 0;
+		}
+		return totalnum;
+	}
+
+	public static void main(String args[]){
+		Vehicle sedan = new Vehicle("tanama");
+		LinkedList c1 = new LinkedList(sedan);
+
+		Vehicle sports_car = new Vehicle("nakata");
+		LinkedList c2 = new LinkedList(sports_car, c1);
+
+		Vehicle truck = new Vehicle("tamana");
+		LinkedList c3 = new LinkedList(truck, c2);
+
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+	}
+}
