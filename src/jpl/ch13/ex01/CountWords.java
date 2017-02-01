1a25ed133a0a8af737186ae5f09ca6d009fbd56e
fumihiro teshima <teshima.java.program@gmail.com>
2017-02-01 19:09:09 +0900

    ch13.ex01,ch13.ex02のバグ修正

--- src/jpl/ch13/ex01/CountWords.java
+++ src/jpl/ch13/ex01/CountWords.java
@@ -5,8 +5,10 @@ public class CountWords {
 	public static int countWordFromString(String str, char word){
 		int index = 0;
 		int count = 0;
-		while((index = str.indexOf(word, index)) != -1)
+		while((index = str.indexOf(word, index)) != -1){
 			count++;
+			index++;
+		}
 		return count;
 	}
 
--- src/jpl/ch13/ex02/CountWords.java
+++ src/jpl/ch13/ex02/CountWords.java
@@ -5,16 +5,20 @@ public class CountWords {
 	public static int countWordFromString(String str, char word){
 		int index = 0;
 		int count = 0;
-		while((index = str.indexOf(word, index)) != -1)
+		while((index = str.indexOf(word, index)) != -1){
 			count++;
+			index++;
+		}
 		return count;
 	}
 
 	public static int countStringFromString(String str, String word){
 		int index = 0;
 		int count = 0;
-		while((index = str.indexOf(word, index)) != -1)
+		while((index = str.indexOf(word, index)) != -1){
 			count++;
+			index++;
+		}
 		return count;
 	}
 }
