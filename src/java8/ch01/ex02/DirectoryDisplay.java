56bce8cd8f94b707b71760748bd84ff32948bee3
fumihiro teshima <teshima.java.program@gmail.com>
2017-06-18 19:38:47 +0900

    add ch01.ex02

--- src/java8/ch01/ex02/DirectoryDisplay.java
+++ src/java8/ch01/ex02/DirectoryDisplay.java
@@ -3,13 +3,16 @@ package java8.ch01.ex02;
 import java.io.File;
 
 public class DirectoryDisplay {
-	public static File[] showDirectriesLambda(String directoryName){
+	public static File[] showDirectriesLambda(String directoryName) {
 		File file = new File(directoryName);
-		return file.listFiles((File subfile, String str) -> {return subfile.isDirectory();});
+		return file.listFiles((subfile) -> {
+			return subfile.isDirectory();
+		});
 	}
 
-	public static File[] showDirectriesMethod(String directoryName){
+	public static File[] showDirectriesMethod(String directoryName) {
 		File file = new File(directoryName);
-		return file.list((File::isDirectory());
+		File[] dirs = file.listFiles(File::isDirectory);
+		return dirs;
 	}
 }
